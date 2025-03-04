53bf7d210fb7581659f384d9c47a5f6fe4018ab6
XiaonanLin <2680697157@qq.com>
2025-03-04 23:58:28 +0800

    feat: 统计字符频率功能

---
+++ 3123004448/Code/src/main/java/pers/duplicatechecking/Frequency.java
@@ -0,0 +1,16 @@
+package pers.duplicatechecking;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Frequency {
+    public static Map<String,Integer> getFrequency(List<String> tokens){
+           Map<String,Integer> freq = new HashMap<>();
+           //如果freq里没有这个词就设为1 如果有就获取频度再加1
+           for (String token : tokens) {
+               freq.put(token, freq.getOrDefault(token, 0) + 1);
+           }
+           return freq;
+    }
+}
