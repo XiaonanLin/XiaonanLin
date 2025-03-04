6c44991a593aa6d9d95341e3ccafee140c27fc78
XiaonanLin <2680697157@qq.com>
2025-03-04 22:50:05 +0800

    feat: 分词功能

---
+++ 3123004448/Code/src/main/java/pers/duplicatechecking/Tokenize.java
@@ -0,0 +1,26 @@
+package pers.duplicatechecking;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenize {
+    public static List<String> tokenize(String text){
+        String cleanedText = text
+                .replaceAll("\\s+", "");
+        //Term是分词结果的一个数据结构
+        List<Term> terms = HanLP.segment(cleanedText);
+        //用来存分词结果
+        List<String> tokens = new ArrayList<>();
+        for (Term term : terms) {
+            //word字段是内容 统一小写
+            String word = term.word.trim().toLowerCase();
+            if (!word.isEmpty()) {
+                tokens.add(word);
+            }
+        }
+        return tokens;
+    }
+}
