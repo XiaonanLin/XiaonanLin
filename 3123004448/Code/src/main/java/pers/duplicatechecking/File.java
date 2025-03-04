7700d66387a240e1b875fc22eed42854f02a410b
XiaonanLin <2680697157@qq.com>
2025-03-04 18:52:56 +0800

    feat: 文件的读取和写入

---
+++ 3123004448/Code/src/main/java/pers/duplicatechecking/File.java
@@ -0,0 +1,28 @@
+package pers.duplicatechecking;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class File {
+      //读取文件
+      public static String readFile(String path) throws IOException {
+            //因为字符流没法指定编码，所以要使用字节流，然后转换成字符流，再将基本流包装成高级流
+            StringBuilder content = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
+                  String line;
+                  while ((line = reader.readLine()) != null) {
+                        content.append(line);
+                  }
+            }
+            return content.toString();
+      }
+
+      //写入文件
+      public static void writeFile(String path, double similarity) throws IOException {
+            try (PrintWriter writer = new PrintWriter(
+                    new OutputStreamWriter(new FileOutputStream(path), "UTF-8"))) {
+                  writer.printf("%.2f", similarity * 100);
+            }
+      }
+}
